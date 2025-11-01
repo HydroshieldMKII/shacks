91db0834a4a9b326062d49296f038b8f0a3ee415
Venorrak <jaudet199@gmail.com>
2025-11-01 11:49:21 -0400

    setup router

--- extension/src/main.tsx
+++ extension/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import Login from './pages/login.tsx'
+import PasswordDetail from './pages/password-details.tsx'
+import PasswordList from './pages/password-list.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/passwords">
+          <Route index element={<PasswordList />} />
+          <Route path=":id" element={<PasswordDetail />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
---
+++ extension/src/pages/password-list.tsx
@@ -0,0 +1,10 @@
+function PasswordList() {
+  return (
+    <div>
+      <h1>Password List</h1>
+      {/* Password list content goes here */}
+    </div>
+  );
+}
+
+export default PasswordList;
